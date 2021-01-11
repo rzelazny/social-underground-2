0531b7112d2f7afdb28fad65162c1019937bdd79
J Rhee <jayrhee0514@gmail.com>
2021-01-11 11:56:50 -0500

    added nav bar, also included previous project casino, home, and members.js into pages

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,13 +1,52 @@
-import React from "react";
+// import React from "react";
 
-function Nav() {
+// function Nav() {
+//   return (
+//     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+//       <a className="navbar-brand" href="/">
+//         React Reading List
+//       </a>
+//     </nav>
+//   );
+// }
+
+// export default Nav;
+
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+
+function NavTabs() {
+  // We'll go into the Hooks API later, for now, we are just using some code
+  // from the react-router docs (https://reacttraining.com/react-router/web/api/Hooks/uselocation)
+  // This allows the component to check the route any time the user uses a link to navigate.
+  const location = useLocation();
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <a className="navbar-brand" href="/">
-        React Reading List
-      </a>
-    </nav>
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+          Home
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/about"
+          className={location.pathname === "/about" ? "nav-link active" : "nav-link"}
+        >
+          Casino
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/blog"
+          className={location.pathname === "/blog" ? "nav-link active" : "nav-link"}
+        >
+          Members
+        </Link>
+      </li>
+    </ul>
   );
 }
 
-export default Nav;
+export default NavTabs;
+
