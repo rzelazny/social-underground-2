11374fdb783a90915e839f008e07430c7822187c
Brianna Hebeler <briannahebeler@gmail.com>
2021-01-13 12:06:19 -0500

    added buttons component and split up where they are being added

--- src/components/BlackjackDirections/index.js
+++ src/components/BlackjackDirections/index.js
@@ -8,6 +8,17 @@ function BlackjackDirections() {
     return (
         <Container id="blackjackDirections">
             <p>Try to get as close to 21 without busting. If you want another card press 'hit' and you will be dealt another card. If you want to stay with your hand and end the game press 'stand'. You can hit as many times as you want but beware, if you bust you lose. To keep playing press 'play another round'. Each round you play, your score will be displayed and will increment as you win. If you tie with the House you will not be awarded any points. If you win you will be awarded 1 point and if the House wins it will be awarded 1 point.</p>
+            <ul>
+                <li id="start">
+                    <a href="#">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span>Start</span>
+                    </a>
+                </li>
+            </ul>
         </Container>
     );
 }
