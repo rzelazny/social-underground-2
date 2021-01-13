895a3558a9085cc608e4428a6d21ba3059f34754
Ryan Zelazny <RyanZelazny@gmail.com>
2021-01-13 14:03:34 -0500

    routing tweaks

--- routes/index.js
+++ routes/index.js
@@ -5,15 +5,21 @@ const htmlRoutes = require("./html-routes");
 
 // API Routes
 console.log("Running API routes");
-router.route("/api", apiRoutes);
-
+router.use("/api", apiRoutes);
+console.log("We've gone too far")
 // HTML Routes
 // console.log("Running HTML routes");
 // router.route("/", htmlRoutes);
 
 // If no API routes are hit, send the React app
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// if (process.env.NODE_ENV === "production") {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// }
+// else{
+  router.use(function(req, res) {
+    res.sendFile(path.join(__dirname, "../client/public/index.html"));
+  });
+//}
+
 
 module.exports = router;
