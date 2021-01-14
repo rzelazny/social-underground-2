3f8fb7d37e619596192a9a188185f060705a56f7
Brianna Hebeler <briannahebeler@gmail.com>
2021-01-14 00:03:27 -0500

    still not getting the hand to show up under the player but I am getting the player data to show up

--- client/src/components/BlackjackHand/index.js
+++ client/src/components/BlackjackHand/index.js
@@ -6,8 +6,7 @@ import API from "../../utils/API";
 
 function BlackjackHand() {
     const [houseHand, setHouseHand] = useState([]);
-    // const [player1Hand, setPlayer1Hand] = useState([]);
-    // const [handArray, setHandArray] = useState([]);
+    const [player1Hand, setPlayer1Hand] = useState([]);
 
     useEffect(() => {
         if (!houseHand) {
@@ -16,7 +15,7 @@ function BlackjackHand() {
 
         API.drawHouse(houseHand)
             .then(data => {
-                console.log(data)
+                // console.log(data)
                 setHouseHand(
                     [
                         {
@@ -37,42 +36,37 @@ function BlackjackHand() {
             // .catch(err => setHouseHand.error(err));
     }, []);
 
-    // useEffect(() => {
-    //     if (!player1Hand) {
-    //         return;
-    //     }
+    useEffect(() => {
+        if (!player1Hand) {
+            return;
+        }
 
-    //     API.drawPlayer1(player1Hand)
-    //             .then(data => {
-    //                 console.log(data)
-    //                 setPlayer1Hand(
-    //                     [
-    //                         {
-    //                             code: data.cards[0].code,
-    //                             suit: data.cards[0].suit,
-    //                             value: data.cards[0].value,
-    //                             imgUrl: data.cards[0].image
-    //                         }, 
-    //                         {
-    //                             code: data.cards[1].code,
-    //                             suit: data.cards[1].suit,
-    //                             value: data.cards[1].value,
-    //                             imgUrl: data.cards[1].image
-    //                         }
-    //                     ]
-    //                 )
-    //             })
-    //             .catch(err => setPlayer1Hand.error(err));
-    // }, []);
-
-    // setHandArray([
-    //     houseHand,
-    //     player1Hand
-    // ])
+        API.drawPlayer1(player1Hand)
+                .then(data => {
+                    // console.log(data)
+                    setPlayer1Hand(
+                    [
+                        {
+                            code: data.data.cards[0].code,
+                            suit: data.data.cards[0].suit,
+                            value: data.data.cards[0].value,
+                            imgUrl: data.data.cards[0].image
+                        }, 
+                        {
+                            code: data.data.cards[1].code,
+                            suit: data.data.cards[1].suit,
+                            value: data.data.cards[1].value,
+                            imgUrl: data.data.cards[1].image
+                        }
+                    ]
+                    )
+                })
+                // .catch(err => setPlayer1Hand.error(err));
+    }, []);
 
     function seeConsole() {
         console.log(houseHand);
-        // console.log("hello"); //works can see in here
+        console.log(player1Hand);
     }
 
     return (
--- client/src/components/BlackjackPlayers/index.js
+++ client/src/components/BlackjackPlayers/index.js
@@ -6,41 +6,41 @@ import BlackjackHand from "../BlackjackHand";
 
 function BlackjackPlayers() {
 
-    // const [house, setHouse] = useState({});
-    // const [player1, setPlayer1] = useState({});
-    // const [playerArray, setPlayerArray] = useState([])
+    const [house, setHouse] = useState({});
+    const [player1, setPlayer1] = useState({});
 
-    // function addPlayers() {
+    function addPlayers(props) {
 
-    //     setHouse({
-    //         Name: 'House', 
-    //         ID: 0, 
-    //         Score: 0, 
-    //         Points: 0, 
-    //         Bust: false, 
-    //         Hand: hand[0], 
-    //         Stand: false
-    //     });
+        setHouse({
+            Name: 'House', 
+            ID: 0, 
+            Score: 0, 
+            Points: 0, 
+            Bust: false, 
+            Hand: props.houseHand, 
+            Stand: false
+        });
     
-    //     setPlayer1({
-    //         Name: 'Player1', 
-    //         ID: 1, 
-    //         Score: 0, 
-    //         Points: 0, 
-    //         Bust: false, 
-    //         Hand: hand[1], 
-    //         Stand: false
-    //     });
-    
-    //     setPlayerArray([
-    //         house,
-    //         player1
-    //     ])
-    // }
+        setPlayer1({
+            Name: 'Player1', 
+            ID: 1, 
+            Score: 0, 
+            Points: 0, 
+            Bust: false, 
+            Hand: props.player1Hand, 
+            Stand: false
+        });
+    }
+
+    function consolePlayers() {
+        console.log(house);
+        console.log(player1);
+    }
 
     return (
         <Container id="players" >
-            <p>The blackjack players will go here.</p>
+            <p onClick={addPlayers}>The blackjack players will go here.</p>
+            <p onClick={consolePlayers}>Click here for console log of players</p>
             <BlackjackHand />
     </Container>
     );
