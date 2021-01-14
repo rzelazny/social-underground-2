551650309173ce83c53cec6c40c070d174ccdc85
Ryan Zelazny <RyanZelazny@gmail.com>
2021-01-14 09:59:13 -0500

    more auth work

--- client/src/App.js
+++ client/src/App.js
@@ -2,27 +2,15 @@ import React, { useState, useEffect }  from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-//import Home from "./pages/Home";
+import Home from "./pages/Home";
 import NoMatch from "./pages/NoMatch";
 import Casino from "./pages/Casino";
 
 import Nav from "./components/Nav/Navbar";
-//import isAuth from "./components/IsAuth/isAuth";
-const isAuth = require("./components/IsAuth/isAuth");
 
 function App() {
 
   const [authenticated, setAuth] = useState([false]);
-
-  fetch("/api/login",{
-    method: "post"
-  })
-  .then((data)=>{
-    console.log("data", data);
-    if(data.status===200) setAuth(true);
-      
-  })
-  .catch((err)=> console.log(err))
 
   return (
     <Router>
@@ -36,8 +24,10 @@ function App() {
             <Signup />
           </Route>
           <Route exact path={"/home"}>
-            {console.log("loggedIn", authenticated)}
-            {authenticated ?  <Casino /> : <Login />}
+            <Home />
+          </Route>
+          <Route exact path={"/casino"}>
+            <Casino />
           </Route>
           <Route>
             <NoMatch />
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,15 @@ const path = require("path");
 const router = require("express").Router();
 const apiRoutes = require("./api-routes");
 
+var isAuthenticated = require("../config/middleware/isAuthenticated");
+
 // API Routes
 console.log("Running API routes");
 router.use("/api", apiRoutes);
 
-  router.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../client/public/index.html"));
-  });
-
-
+router.get("/home", isAuthenticated, function (req, res) {
+  console.log("sending them to :" + req.url);
+  res.sendFile(path.join(__dirname, "../client/public/index.html"));
+});
 
 module.exports = router;
