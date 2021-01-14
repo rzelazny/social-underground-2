2a73a7791b141c4d81b54191dbb054e8190d4aee
Brianna Hebeler <briannahebeler@gmail.com>
2021-01-14 11:15:29 -0500

    added buttons to players component for testing purposes // created function to add points to hand

--- client/src/components/BlackjackPlayers/index.js
+++ client/src/components/BlackjackPlayers/index.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import "./style.css"
 import { Container } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import BlackjackHand from "../BlackjackHand";
 import API from "../../utils/API";
 
 
@@ -10,8 +9,12 @@ function BlackjackPlayers() {
 
     const [house, setHouse] = useState({});
     const [player1, setPlayer1] = useState({});
+
     const [houseHand, setHouseHand] = useState([]);
     const [player1Hand, setPlayer1Hand] = useState([]);
+
+    const [housePoints, setHousePoints] = useState();
+    const [player1Points, setPlayer1Points] = useState();
 
     useEffect(() => {
         if (!houseHand) {
@@ -69,14 +72,23 @@ function BlackjackPlayers() {
                 // .catch(err => setPlayer1Hand.error(err));
     }, []);
 
+    function updatePoints() {
+        setHousePoints(
+            parseInt(houseHand[0].value) + parseInt(houseHand[1].value)
+        );
+        setPlayer1Points(
+            parseInt(player1Hand[0].value) + parseInt(player1Hand[1].value)
+        )
+    }
 
-    function addPlayers(props) {
+
+    function addPlayers() {
 
         setHouse({
             Name: 'House', 
             ID: 0, 
             Score: 0, 
-            Points: 0, 
+            Points: housePoints, 
             Bust: false, 
             Hand: houseHand, 
             Stand: false
@@ -86,7 +98,7 @@ function BlackjackPlayers() {
             Name: 'Player1', 
             ID: 1, 
             Score: 0, 
-            Points: 0, 
+            Points: player1Points, 
             Bust: false, 
             Hand: player1Hand,
             Stand: false
@@ -96,15 +108,13 @@ function BlackjackPlayers() {
     function consolePlayers() {
         console.log(house);
         console.log(player1);
-        console.log(houseHand);
-        console.log(player1Hand);
     }
 
     return (
-        <Container id="players" >
-            <p onClick={consolePlayers}>Click here for console log of players.</p>
-            <p onClick={addPlayers}>Click here to add players.</p>
-            {/* <BlackjackHand /> */}
+        <Container id="players">
+            <button onClick={updatePoints} >update points</button>
+            <button onClick={addPlayers} >add players</button>
+            <button onClick={consolePlayers} >see console log</button>
     </Container>
     );
 }
