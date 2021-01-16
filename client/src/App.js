a67447e8763c167f8800d56bb77f12cf28068bea
Brianna Hebeler <briannahebeler@gmail.com>
2021-01-16 02:26:02 -0500

    fixing merge conflic in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,40 +1,40 @@
 import React, { useState, useEffect }  from "react";
-// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-// import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// import Signup from "./pages/Signup";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 import Casino from "./pages/Casino";
-// import NoMatch from "./pages/NoMatch";
-// import Nav from "./components/Nav/Navbar";
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav/Navbar";
 
 function App() {
 
-  // const [authenticated, setAuth] = useState([false]);
+  const [authenticated, setAuth] = useState([false]);
 
   return (
-    // <Router>
-    //   <div>
-    //     <Nav />
-    //     <Switch>
-    //       <Route exact path={["/", "/login"]}>
-    //         <Login />
-    //       </Route>
-    //       <Route exact path={"/signup"}>
-    //         <Signup />
-    //       </Route>
-    //       <Route exact path={"/home"}>
-    //         <Home />
-    //       </Route>
-    //       <Route exact path={"/casino"}>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          <Route exact path={["/", "/login"]}>
+            <Login />
+          </Route>
+          <Route exact path={"/signup"}>
+            <Signup />
+          </Route>
+          <Route exact path={"/home"}>
+            <Home />
+          </Route>
+          <Route exact path={"/casino"}>
             <Casino />
-    //       </Route>
-    //       <Route>
-    //         <NoMatch />
-    //       </Route>
-    //       <Route path ="/casino" component = {Casino} />
-    //     </Switch>
-    //   </div>
-    // </Router>
+          </Route>
+          <Route>
+            <NoMatch />
+          </Route>
+          <Route path ="/casino" component = {Casino} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
