67df11f5eb0663b784547640c0406979882ab583
Brianna Hebeler <briannahebeler@gmail.com>
2021-01-19 13:40:52 -0500

    added some styling to ww directions

--- client/src/components/WerewolfGame/index.js
+++ client/src/components/WerewolfGame/index.js
@@ -1,19 +1,36 @@
 import React, { useState } from "react";
 import "./style.css"
-import { Container, Card, Button } from 'reactstrap';
+import { Container, Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function WerewolfGame() {
 
+    const [displayDirections, setDisplayDirections] = useState(true);
+    const [startGame, setStartGame] = useState(false);
+
     // [players, setPlayers] = setState();
 
+    function onStart() {
+        console.log("clicking start btn");
+        setStartGame(true);
+        setDisplayDirections(false);
+    }
+
     return(
-        <Container>
-            <Card id="directions">
-                <h2>Werewolf</h2>
-                <p>some basic instructions will go here</p>
-                <Button>Start</Button>
-            </Card>
+        <Container id="werewolfTable">
+            <h2>Werewolf</h2>
+            <br />
+                {displayDirections
+                && 
+                <div id="directions">
+                    <p>some basic instructions will go here</p>
+                    <Button color="danger" onClick={onStart}>Start</Button>
+                </div>
+                }
+                {startGame 
+                && 
+                <p>The game will display here</p> 
+                }
         </Container>
     )
 }
