75681d44b0e6c6da3995660b05cafc0518c2e53a
Brianna Hebeler <briannahebeler@gmail.com>
2021-01-19 15:23:09 -0500

    added card data for other players // a little css styling

--- client/src/components/WerewolfGame3/index.js
+++ client/src/components/WerewolfGame3/index.js
@@ -11,7 +11,6 @@ var player3Name = "Player 3";
 function WerewolfGame3() {
     //character options //
     var threeOptions = ["townsfolk", "townsfolk", "beast" ];
-    // console.log(threeOptions);
 
     //shuffle the array here //
     function shuffleArray(array) {
@@ -21,21 +20,20 @@ function WerewolfGame3() {
         }
     }
     shuffleArray(threeOptions);
-    // console.log(threeOptions);
 
     var player1Character = threeOptions[0];
     var player2Character = threeOptions[1];
     var player3Character = threeOptions[2];
 
     var townsfolkInstructions = "put townsfolk instructions here";
-    var beastInstructions = "put townsfolk instructions here";
+    var beastInstructions = "put beast instructions here";
 
     return(
-        <Container>
-            <p>3 player game goes here</p>
-            <Card id="player1">
-                <CardTitle>{player1Character}</CardTitle>
-                <CardSubtitle>{player1Name}</CardSubtitle>
+        <Container className="werewolfGame3">
+
+            <Card id="play1" className="werewolfGame3">
+                <CardSubtitle tag="h6" className="mb-2 text-muted">{player1Name}</CardSubtitle>
+                <CardTitle tag="h2" >{player1Character}</CardTitle>
                 <CardText>
                     {
                     player1Character === "townsfolk"
@@ -48,17 +46,53 @@ function WerewolfGame3() {
                     beastInstructions
                     }
                 </CardText>
-                <Button>Vote to kill {player2Name} </Button>
-                <Button>Vote to kill {player3Name} </Button>
+                <div>
+                    <Button color="danger">Vote to kill {player2Name}</Button>{' '}
+                    <Button color="danger">Vote to kill {player3Name}</Button>{' '}
+                </div>
             </Card>
 
-            {/* // 3 cards (one for each player -- will only display to that player)
-            // username
-            // character assigned
-            // instructions -- players can chat 
-            // vote to kill buttons */}
-            <Card id="player2"></Card>
-            <Card id="player3"></Card>
+            <Card id="play2" className="werewolfGame3">
+                <CardSubtitle tag="h6" className="mb-2 text-muted">{player2Name}</CardSubtitle>
+                <CardTitle tag="h2" >{player2Character}</CardTitle>
+                <CardText>
+                    {
+                    player2Character === "townsfolk"
+                    &&
+                    townsfolkInstructions
+                    }
+                    {
+                    player2Character === "beast"
+                    &&
+                    beastInstructions
+                    }
+                </CardText>
+                <div>
+                    <Button color="danger">Vote to kill {player1Name}</Button>{' '}
+                    <Button color="danger">Vote to kill {player3Name}</Button>{' '}
+                </div>
+            </Card>
+
+            <Card id="play3" className="werewolfGame3">
+                <CardSubtitle tag="h6" className="mb-2 text-muted">{player3Name}</CardSubtitle>
+                <CardTitle tag="h2" >{player3Character}</CardTitle>
+                <CardText>
+                    {
+                    player3Character === "townsfolk"
+                    &&
+                    townsfolkInstructions
+                    }
+                    {
+                    player3Character === "beast"
+                    &&
+                    beastInstructions
+                    }
+                </CardText>
+                <div>
+                    <Button color="danger">Vote to kill {player1Name}</Button>{' '}
+                    <Button color="danger">Vote to kill {player2Name}</Button>{' '}
+                </div>
+            </Card>
         </Container>
     )
 }
