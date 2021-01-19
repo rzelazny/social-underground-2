9c82e252f1c15bd879091e3a6d397af82d97f16f
Brianna Hebeler <briannahebeler@gmail.com>
2021-01-19 13:13:18 -0500

    reformatted game choice comp

--- client/src/components/GameChoice/index.js
+++ client/src/components/GameChoice/index.js
@@ -7,8 +7,8 @@ import RPSTable from "../RPSTable";
 import WerewolfGame from "../WerewolfGame";
 
 function GamingTable() {
-    const [formDisplay, setFormDisplay] = useState(true);
 
+    const [formDisplay, setFormDisplay] = useState(true);
     const [displayGame, setDisplayGame] = useState();
 
     const [multi, setMulti] = useState(false);
@@ -16,17 +16,23 @@ function GamingTable() {
 
     const [players, setPlayers] = useState();
 
+    function ifSingle() {
+        setSingle(true);
+        setMulti(false);
+    }
 
-    // function handleSubmit(event) {
-    //     event.preventDefault();
-    //     setFormDisplay(false);
-    //     if(this.state.value === "blackjackGame") {
-    //         setDisplayGame("blackjack");
-    //     }
-    //     else if(this.state.vale === "rpsGame") {
-    //         setDisplayGame("rps");
-    //     }
-    // }
+    function ifMulti() {
+        setMulti(true);
+        setSingle(false);
+    }
+
+    function twoPlayers() {
+        setPlayers("two")
+    }
+
+    function threePlusPlayers() {
+        setPlayers("threePlus")
+    }
 
     function blackjack() {
         setFormDisplay(false);
@@ -43,119 +49,99 @@ function GamingTable() {
         setDisplayGame("werewolf");
     }
 
-    function twoPlayers() {
-        setPlayers("two")
-    }
-    function threePlusPlayers() {
-        setPlayers("threePlus")
-    }
-
-    function ifMulti() {
-        setMulti(true);
-        setSingle(false);
-    }
-
-
-
-    function ifSingle() {
-        setSingle(true);
-        setMulti(false);
-    }
-
-
     return (
         <Container className="text-center">
             {formDisplay
-            &&
-            <Card className="text-center mx-auto gameChoice">
-                <CardTitle tag="h3">Game Mode</CardTitle>
-                <Form 
-                // onSubmit={handleSubmit}
-                >
-                    <FormGroup tag="fieldset">
-                        <FormGroup check className="form-check-inline">
-                            <Label check>
-                                <Input 
-                                type="radio" 
-                                name="radio1"
-                                onClick={ifMulti}
-                                />{' '}
+                &&
+                <Card className="text-center mx-auto gameChoice">
+                    <CardTitle tag="h3">Game Mode</CardTitle>
+                    <Form
+                    // onSubmit={handleSubmit}
+                    >
+                        <FormGroup tag="fieldset">
+                            <FormGroup check className="form-check-inline">
+                                <Label check>
+                                    <Input
+                                        type="radio"
+                                        name="radio1"
+                                        onClick={ifMulti}
+                                    />{' '}
                                 Multiplayer
                             </Label>
-                        </FormGroup>
-                        <FormGroup check className="form-check-inline">
-                            <Label check>
-                                <Input 
-                                type="radio" 
-                                name="radio1" 
-                                onClick={ifSingle}
-                                />{' '}
+                            </FormGroup>
+                            <FormGroup check className="form-check-inline">
+                                <Label check>
+                                    <Input
+                                        type="radio"
+                                        name="radio1"
+                                        onClick={ifSingle}
+                                    />{' '}
                                 Single Player
                             </Label>
+                            </FormGroup>
                         </FormGroup>
-                    </FormGroup>
-                    <br />
+                        <br />
 
-                    {single
-                    &&
-                    <CardTitle tag="h3">Choose a game</CardTitle>
-                    }
-                    {single
-                    && <Button color="danger" onClick={blackjack}>Blackjack</Button>
-                    }
+                        {single
+                            &&
+                            <CardTitle tag="h3">Choose a game</CardTitle>
+                        }
+                        {single
+                            && <Button color="danger" onClick={blackjack}>Blackjack</Button>
+                        }
 
-                    {multi 
-                    &&
-                    <CardTitle tag="h3">How many players?</CardTitle>
-                    }
-                    {multi 
-                    &&
-                    <Form>
-                    <FormGroup tag="fieldset">
-                        <FormGroup check className="form-check-inline">
-                            <Label check>
-                                <Input 
-                                type="radio" 
-                                name="radio1"
-                                onClick={twoPlayers}
-                                />{' '}
+                        {multi
+                            &&
+                            <CardTitle tag="h3">How many players?</CardTitle>
+                        }
+                        {multi
+                            &&
+                            <Form>
+                                <FormGroup tag="fieldset">
+                                    <FormGroup check className="form-check-inline">
+                                        <Label check>
+                                            <Input
+                                                type="radio"
+                                                name="radio1"
+                                                onClick={twoPlayers}
+                                            />{' '}
                                 2
                             </Label>
-                        </FormGroup>
-                        <FormGroup check className="form-check-inline">
-                            <Label check>
-                                <Input 
-                                type="radio" 
-                                name="radio1" 
-                                onClick={threePlusPlayers}
-                                />{' '}
+                                    </FormGroup>
+                                    <FormGroup check className="form-check-inline">
+                                        <Label check>
+                                            <Input
+                                                type="radio"
+                                                name="radio1"
+                                                onClick={threePlusPlayers}
+                                            />{' '}
                                 3+
                             </Label>
-                        </FormGroup>
-                    </FormGroup>
-                    </Form>
-                    }
-                    {players
-                    &&
-                    <CardTitle tag="h3">Choose a game</CardTitle>
-                    }
-
-                    {players === "two"
-                        &&
-                        <Button color="danger" onClick={rps}>Rock Paper Scissors</Button>
+                                    </FormGroup>
+                                </FormGroup>
+                            </Form>
                         }
-                            {players === "threePlus"
+                        {players
+                            &&
+                            <CardTitle tag="h3">Choose a game</CardTitle>
+                        }
+
+                        {players === "two"
+                            &&
+                            <Button color="danger" onClick={rps}>Rock Paper Scissors</Button>
+                        }
+                        {players === "threePlus"
                             && <Button color="danger" onClick={werewolf}>Werewolf</Button>
-                            }
-                </Form>
-            </Card>
+                        }
+                    </Form>
+                </Card>
             }
             {displayGame === "blackjack"
-            && <BlackjackTable />}
+                && <BlackjackTable />}
             {displayGame === "rps"
-            && <RPSTable />}
+                && <RPSTable />}
             {displayGame === "werewolf"
-            && <WerewolfGame />}
+                && <WerewolfGame />}
         </Container>
     );
 }
