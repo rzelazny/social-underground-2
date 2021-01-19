c32ac67771fcafe0e7327e4eca29a0b6944b89cd
Brianna Hebeler <briannahebeler@gmail.com>
2021-01-19 15:03:16 -0500

    on load of component the array is shuffled and characters are assigned to players

--- client/src/components/WerewolfGame3/index.js
+++ client/src/components/WerewolfGame3/index.js
@@ -1,35 +1,43 @@
 import React, { useState } from "react";
 import "./style.css"
-import { Container } from 'reactstrap';
+import { Container, Button, CarouselControl } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-var player1 = "Player 1";
-var player2 = "Player 2";
-var player3 = "Player 3";
-
-//character options //
-var threeOptions = ["townsfolk", "townsfolk", "beast" ];
+//hard coded players from now but pull from db//
+var player1Name = "Player 1";
+var player2Name = "Player 2";
+var player3Name = "Player 3";
 
 function WerewolfGame3() {
+    //character options //
+    var threeOptions = ["townsfolk", "townsfolk", "beast" ];
+    // console.log(threeOptions);
+
+    //shuffle the array here //
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+    shuffleArray(threeOptions);
+    // console.log(threeOptions);
+
+    var player1Character = threeOptions[0];
+    var player2Character = threeOptions[1];
+    var player3Character = threeOptions[2];
+
     return(
         <Container>
             <p>3 player game goes here</p>
+            {/* <Button>set characters</Button> */}
+            {/* // 3 cards (one for each player -- will only display to that player)
+            // username
+            // character assigned
+            // instructions -- players can chat 
+            // vote to kill buttons */}
         </Container>
     )
 }
 
 export default WerewolfGame3;
-
-
-
-
-        //randomly assign characters here //
-        // generate cards for each player
-
-        // 3 cards (one for each player -- will only display to that player)
-                // username
-                // character assigned
-                // instructions -- players can chat 
-                // vote to kill buttons
-
-                //hard coded players from now but pull from db//
