acc2d7a4721ed20cb93080dd653706ac5b056a45
J Rhee <jayrhee0514@gmail.com>
2021-01-19 11:53:58 -0500

    created a workable image upload

--- client/src/components/Members/PhotoUpload.js
+++ client/src/components/Members/PhotoUpload.js
@@ -1,34 +1,59 @@
-import React from 'react'
-import ImageUploader from "react-images-upload"
+import React from "react";
+import "./PhotoUpload.css"
 
 class PhotoUpload extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {file: '',imagePreviewUrl: ''};
+  }
 
-    constructor(props){
-        super(props);
-        this.state = {pictures: []}
-        this.onDrop = this.onDrop.bind(this)
+  _handleSubmit(e) {
+    e.preventDefault();
+    // TODO: do something with -> this.state.file
+    console.log('handle uploading-', this.state.file);
+  }
+
+  _handleImageChange(e) {
+    e.preventDefault();
+
+    let reader = new FileReader();
+    let file = e.target.files[0];
+
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imagePreviewUrl: reader.result
+      });
     }
 
-    onDrop(picture) {
-        this.setState({
-            pictures: this.state.pictures.concat(picture)
-        });
+    reader.readAsDataURL(file)
+  }
+
+  render() {
+    let {imagePreviewUrl} = this.state;
+    let $imagePreview = null;
+    if (imagePreviewUrl) {
+      $imagePreview = (<img src={imagePreviewUrl} />);
+    } else {
+      $imagePreview = (<div className="previewText">Please select an Image for Preview</div>);
     }
 
-    render() {
-        return (
-            <div>
-            <ImageUploader 
-                withIcon={false}
-                buttonText= "Choose Image"
-                onChange={this.onDrop}
-                imgExtension={[".jpg", ".png", ".gif"]}
-                maxFileSize={5224880}
-                withPreview={true}
-            />
-            </div>
-        )
-    }
+    return (
+      <div className="previewComponent">
+        <form onSubmit={(e)=>this._handleSubmit(e)}>
+          <input className="fileInput" 
+            type="file" 
+            onChange={(e)=>this._handleImageChange(e)} />
+          <button className="submitButton" 
+            type="submit" 
+            onClick={(e)=>this._handleSubmit(e)}>Upload Image</button>
+        </form>
+        <div className="imgPreview">
+          {$imagePreview}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default PhotoUpload
