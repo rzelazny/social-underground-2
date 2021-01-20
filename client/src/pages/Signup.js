8ed49e7a96ea0ec3beaecf99427c189bd92d3ebe
Ryan Zelazny <RyanZelazny@gmail.com>
2021-01-19 20:12:09 -0500

    navbar home and signup page now correctly go to home page not login

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -51,7 +51,7 @@ function Signup() {
         })
             .then(function (data) {
                 console.log(data);
-                //window.location.replace("/home");
+                window.location.replace("/home");
                 // If there's an error, handle it by throwing up a bootstrap alert
             })
             .catch(handleLoginErr);
