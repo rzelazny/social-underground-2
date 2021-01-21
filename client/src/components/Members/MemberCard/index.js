085bca9ed6cedbe704a27b0195aec34f00a08a38
Brianna Hebeler <briannahebeler@gmail.com>
2021-01-20 20:53:30 -0500

    pull

--- client/src/components/Members/MemberCard/index.js
+++ client/src/components/Members/MemberCard/index.js
@@ -19,50 +19,6 @@ function MemberCard() {
     }
     stats();
 
-    // function MyComponent() {
-    //     const [error, setError] = useState(null);
-    //     const [isLoaded, setIsLoaded] = useState(false);
-    //     const [items, setItems] = useState([]);
-
-    //     // Note: the empty deps array [] means
-    //     // this useEffect will run once
-    //     // similar to componentDidMount()
-    //     useEffect(() => {
-    //         fetch("/api/UserStats")
-    //             .then(res => res.json())
-    //             .then(
-    //                 (result) => {
-    //                     setIsLoaded(true);
-    //                     setItems(result);
-    //                 },
-    //                 // Note: it's important to handle errors here
-    //                 // instead of a catch() block so that we don't swallow
-    //                 // exceptions from actual bugs in components.
-    //                 (error) => {
-    //                     setIsLoaded(true);
-    //                     setError(error);
-    //                 }
-    //             )
-    //     }, [])
-
-    //     if (error) {
-    //         return <div>Error: {error.message}</div>;
-    //     } else if (!isLoaded) {
-    //         return <div>Loading...</div>;
-    //     } else {
-    //         return (
-    //             <ul>
-    //                 {items.map(item => (
-    //                     <li key={item.id}>
-    //                         {item.name} 
-    //                     </li>
-    //                 ))}
-    //             </ul>
-    //         );
-    //     }
-    // }
-    // MyComponent();
-
     return (
         <div className="cards">
             <h1>Your Stats</h1>
