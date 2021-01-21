e8aa675db541c79fad4e1bf2d105582fa2705955
Ryan Zelazny <RyanZelazny@gmail.com>
2021-01-20 21:05:27 -0500

    signup takes you to login without flickering to home first

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -58,7 +58,8 @@ function Signup() {
         })
             .then(function (data) {
                 console.log(data);
-                window.location.replace("/home");
+                
+                window.location.replace("/login");
                 // If there's an error, handle it by throwing up a bootstrap alert
             })
             .catch(handleLoginErr);
